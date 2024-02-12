df469611afad26af74f9be9036256d326765c826
Noddy09032001 <niranjan.dhokarikar19@vit.edu>
2024-02-12 17:44:04 +0530

    Added code for service layers

---
+++ backend/src/main/java/com/app/service/TicketService.java
@@ -0,0 +1,6 @@
+import com.example.FinalCodeVehicle.entity.Ticket;
+import com.example.FinalCodeVehicle.entity.Vehicle;
+
+public interface TicketService {
+    public Ticket parkVehicle(Vehicle newVehicle); // method to park the new vehicle
+}
